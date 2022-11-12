deeea0aedb86ab78e4cd1f883544bbf13c7b39e7
yanness168 <94063697+yanness168@users.noreply.github.com>
2022-11-12 10:11:19 -0500

    done

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef} from 'react';
+import { SafeAreaView, Animated, PanResponder, StyleSheet, View } from "react-native";
+
+
 
 export default function App() {
+  /* Create an animated position */
+  const initial = useRef(new Animated.ValueXY()).current;
+  const value = {x: 0, y:0}
+  /* Create a gesture responder sys */
+  const Pan = PanResponder.create({
+    /* Claim to be the the responder (just the assigned component) */
+      onStartShouldSetPanResponder: (e,gesture) => true,
+      /* When the user touches, set x and y to (0,0)*/
+      onPanResponderGrant:(e,gesture)=>{
+        initial.setOffset({
+          x: value.x,
+          y: value.y
+        })
+      },
+      /* When user dragging the shape, map dx and dy to x & y */
+      onPanResponderMove: Animated.event([
+        null, {dx: initial.x,dy: initial.y},
+      ]),
+      /* When user releases his finger, let the shape goes back to (0,0) */
+      onPanResponderRelease: () => {
+        Animated.timing(initial,{
+            toValue: {x:0, y:0},
+            duration: 350,
+          }
+        ).start(()=>{
+          alert('You released me!')
+        });
+      }
+    })
+    
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+                                        {/* Converts {x, y} into {left, top} for use in style */}
+      <Animated.View {...Pan.panHandlers} style={[initial.getLayout(), styles.child]}/>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +49,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  child: {
+    backgroundColor: "pink",
+    width: 200,
+    height: 200,
+    borderRadius: 4,
   },
 });
